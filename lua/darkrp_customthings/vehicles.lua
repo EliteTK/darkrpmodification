eb51a5cf7449f0f163f8a7736ffcd2db0f8f037a
markrjg <mark.annable@hotmail.co.uk>
2014-02-02 20:26:33 +0000

    added test vehicles

--- lua/darkrp_customthings/vehicles.lua
+++ lua/darkrp_customthings/vehicles.lua
@@ -12,3 +12,23 @@ http://wiki.darkrp.com/index.php/DarkRP:Vehicles
 
 Add vehicles under the following line:
 ---------------------------------------------------------------------------*/
+DarkRP.createVehicle({
+        name = "M1 Abrams",
+        model = "models/tiggomods/tanks/m1a1/body.mdl",
+        price = 3500000,
+        allowed = {TEAM_CAR_DEALER}
+})
+
+DarkRP.createVehicle({
+        name = "Yamahay FZ450",
+        model = "models/tiggogm/vehicles/yamahayfz450.mdl",
+        price = 2000,
+        allowed = {TEAM_CAR_DEALER}
+})
+
+DarkRP.createVehicle({
+        name = "Taxi",
+        model = "models/tiggogm/vehicles/yamahayfz450.mdl",
+        price = 500,
+        allowed = {TEAM_TAXI}
+})
