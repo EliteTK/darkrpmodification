6350e5cf2f1886222d0ac1a9560493e51f80705c
markrjg <mark.annable@hotmail.co.uk>
2014-02-02 20:54:49 +0000

    Update vehicles.lua

--- lua/darkrp_customthings/vehicles.lua
+++ lua/darkrp_customthings/vehicles.lua
@@ -16,6 +16,7 @@ DarkRP.createVehicle({
         name = "Abrams",
         model = "models/tiggomods/tanks/m1a1/body.mdl",
         price = 3500000,
+        color = Color(0, 140, 0, 255),
         allowed = {TEAM_CAR_DEALER},
         label = "M1 Abrams",
 })
@@ -35,4 +36,60 @@ DarkRP.createVehicle({
         price = 500,
         allowed = {TEAM_TAXI},
         label = "Taxi",
+})
+
+DarkRP.createVehicle({
+        name = "Humvee",
+        model = "models/tiggomods/vehicles/humvee.mdl",
+        price = 3000,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Humvee",
+})
+
+DarkRP.createVehicle({
+        name = "1996 Chevrolet Corvette",
+        model = "models/tiggomods/vehicles/1996corvette.mdl",
+        price = 2750,
+        allowed = {TEAM_CAR_DEALER},
+        label = "1996 Chevrolet Corvette",
+})
+
+DarkRP.createVehicle({
+        name = "Junker1",
+        model = "models/props_vehicles/car002b.mdl",
+        price = 500,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Junker 1",
+})
+
+DarkRP.createVehicle({
+        name = "Junker2",
+        model = "models/props_vehicles/car003b.mdl",
+        price = 500,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Junker 2",
+})
+
+DarkRP.createVehicle({
+        name = "Ferrari 360 Spider",
+        model = "models/tiggomods/vehicles/ferrari360.mdl",
+        price = 3500,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Ferrari 360",
+})
+
+DarkRP.createVehicle({
+        name = "Brera",
+        model = "models/tiggomods/vehicles/brera.mdl",
+        price = 2500,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Brera",
+})
+
+DarkRP.createVehicle({
+        name = "impala88",
+        model = "models/splayn/impala88_sep.mdl",
+        price = 2500,
+        allowed = {TEAM_CAR_DEALER},
+        label = "Impala 88",
 })
