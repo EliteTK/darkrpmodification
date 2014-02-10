55f3581e017d5ccfabb760774490d1f90dd05dbc
markrjg <mark.annable@hotmail.co.uk>
2014-02-10 20:50:10 +0000

    Update shared.lua

--- lua/entities/novice_printer/shared.lua
+++ lua/entities/novice_printer/shared.lua
@@ -1,9 +1,9 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
-ENT.PrintName = "Money Printer"
-ENT.Author = "DarkRP Developers and <enter name here>"
+ENT.PrintName = "Novice Money Printer"
+ENT.Author = "DarkRP Developers and Mark.Annable"
 ENT.Spawnable = false
-ENT.AdminSpawnable = false
+ENT.AdminSpawnable = true
 
 function ENT:SetupDataTables()
 self:NetworkVar("Int", 0, "price")
