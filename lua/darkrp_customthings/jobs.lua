a4039657459bc7abbd33bc55bdafa1a91f1c9880
markrjg <mark.annable@hotmail.co.uk>
2014-02-11 21:51:33 +0000

    Update jobs.lua

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -25,7 +25,7 @@ TEAM_CITIZEN = DarkRP.createJob("Citizen", {
 	weapons = {},
 	command = "citizen",
 	max = 0,
-	salary = function(ply, price) return ply:GetUserGroup() == "donator" and 90 or 45 end,
+	salary = function(ply, salary) return ply:GetUserGroup() == "donator" and 90 or 45 end,
 	admin = 0,
 	vote = false,
 	hasLicense = false,
