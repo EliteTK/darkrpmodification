33a01c5014014a468b58f4d9142789e8307113b8
markrjg <mark.annable@hotmail.co.uk>
2014-02-15 13:51:33 +0000

    test

--- lua/darkrp_customthings/jobs.lua
+++ lua/darkrp_customthings/jobs.lua
@@ -30,6 +30,7 @@ TEAM_CITIZEN = DarkRP.createJob("Citizen", {
 	vote = false,
 	hasLicense = false,
 	candemote = false
+	customCheck = function(ply, salary) return ply:GetUserGroup() == "donator" and salary * 2 or salary end,
 })
 
 TEAM_GANG = DarkRP.createJob("Gangster", {
