4b9fefd7251df5661d0cb43a4e6bf701f40994ce
markrjg <mark.annable@hotmail.co.uk>
2014-02-18 19:07:13 +0000

    Update cl_huddesign1.lua

--- lua/darkrp_modules/marks_hud_design1/cl_huddesign1.lua
+++ lua/darkrp_modules/marks_hud_design1/cl_huddesign1.lua
@@ -28,54 +28,60 @@ hook.Add("HUDShouldDraw", "HideDefaultDarkRPHud", function(name)
 if hideHUDElements[name] then return false end
 end)
 
+
 /*---------------------------------------------------------------------------
 Mark's HUD DESIGN 1
 Made by mark
 ---------------------------------------------------------------------------*/
 
 local function paintHud()
+local x = ScrW()/1680
+local y = ScrH()/1050
+local invx = 1/x
+local invy= 1/y
+
 	-- background
-	draw.RoundedBox(10, 10, ScrH()-135, 450, 125, Color(0, 0, 0, 200))
+	draw.RoundedBox(10 , 10 * x, ScrH()-(135 * y), 450 * x, 125 * y, Color(0, 0, 0, 200))
 	       
 	-- Health 
-	draw.RoundedBox(10, 15, ScrH() - 70, 435, 25, Color(0,0,0,200))
+	draw.RoundedBox(10, 15 * x, ScrH() - (70 * y), 435 * x, 25 * y, Color(0,0,0,200))
 	local Health = LocalPlayer():Health()
     local DrawHealth = math.Min(Health / GAMEMODE.Config.startinghealth, 1)
     if Health != 0 then
-        draw.RoundedBox(10, 17, ScrH() - 68, 431 * DrawHealth, 21, Color(255,0,0,100))
+        draw.RoundedBox(10, 17 * x, ScrH() - (68 * y), 431 * DrawHealth * x, 21 * y, Color(255,0,0,100))
     end
 	if Health < 0 then
 	Health = 0
 	end
-    draw.DrawText("Health: " .. Health, "HudSelectionText",235, ScrH() - 65, Color(255,255,255,255), TEXT_ALIGN_CENTER)
+    draw.DrawText("Health: " .. Health, "HudSelectionText",235 * x, ScrH() - (65 * y), Color(255,255,255,255), TEXT_ALIGN_CENTER)
        
     -- Armor
-    draw.RoundedBox(10, 15, ScrH() - 40, 435, 25, Color(0,0,0,200))
+    draw.RoundedBox(10, 15 * x, ScrH() - (40 * y), 435 * x, 25 * y, Color(0,0,0,200))
        
     local armor = LocalPlayer():Armor()
     if armor != 0 then
-	    draw.RoundedBox(10, 17, ScrH() - 38, 431 * armor / 100, 21, Color(0,130,255,100))
+	    draw.RoundedBox(10, 17 * x, ScrH() - (38 * y), (431 * armor / 100) * x, 21 * y, Color(0,130,255,100))
         end
-    draw.DrawText("Armor: " .. armor, "HudSelectionText",235, ScrH() - 35, Color(255,255,255,255), TEXT_ALIGN_CENTER)
+    draw.DrawText("Armor: " .. armor, "HudSelectionText",235 * x, ScrH() - (35 * y), Color(255,255,255,255), TEXT_ALIGN_CENTER)
 	
 	-- Name
-	draw.RoundedBox(10, 15, ScrH()-130, 200, 25, Color(0, 0, 0, 200))
-	draw.DrawText(LocalPlayer():Nick(), "HudSelectionText", 115, ScrH()-125, Color(255,255,255,255),TEXT_ALIGN_CENTER)
+	draw.RoundedBox(10, 15 * x, ScrH()- (130 * y), 200 * x, 25 * y, Color(0, 0, 0, 200))
+	draw.DrawText(LocalPlayer():Nick(), "HudSelectionText", 115 * x, ScrH()- (125 * y), Color(255,255,255,255),TEXT_ALIGN_CENTER)
 	
 	-- Wallet
 	
     local Wallet = DarkRP.getPhrase("wallet", GAMEMODE.Config.currency, (LocalPlayer():getDarkRPVar("money") or 0))
-    draw.RoundedBox(10, 250, ScrH()-130, 200, 25, Color(0,0,0,200))
-    draw.DrawText(Wallet, "HudSelectionText", 350, ScrH()-125, Color(255,255,255,255), TEXT_ALIGN_CENTER)
+    draw.RoundedBox(10, 250 * x, ScrH()- (130 * y), 200 * x, 25 * y, Color(0,0,0,200))
+    draw.DrawText(Wallet, "HudSelectionText", 350 * x, ScrH()- (125 * y), Color(255,255,255,255), TEXT_ALIGN_CENTER)
 	
 	-- Job
     local Job = DarkRP.getPhrase("job", LocalPlayer():getDarkRPVar("job") or "")
-    draw.RoundedBox(10, 15, ScrH() - 100, 200, 25, team.GetColor(LocalPlayer():Team()))
-    draw.DrawText(Job, "HudSelectionText", 115, ScrH() - 95, Color(255,255,255,255), TEXT_ALIGN_CENTER)
+    draw.RoundedBox(10, 15 * x, ScrH() - (100 * y), 200 * x, 25 * y, team.GetColor(LocalPlayer():Team()))
+    draw.DrawText(Job, "HudSelectionText", 115 * x, ScrH() - (95 * y), Color(255,255,255,255), TEXT_ALIGN_CENTER)
 	
 	-- Salary
 	local Salary = DarkRP.getPhrase("salary", GAMEMODE.Config.currency, (LocalPlayer():getDarkRPVar("salary") or 0))
-	draw.RoundedBox(10, 250, ScrH()-100, 200, 25, Color(0, 0, 0, 200))
-	draw.DrawText(Salary, "HudSelectionText", 350, ScrH()-95, Color(255,255,255,255),TEXT_ALIGN_CENTER)
+	draw.RoundedBox(10, 250 * x, ScrH()- (100 * y), 200 * x, 25 * y, Color(0, 0, 0, 200))
+	draw.DrawText(Salary, "HudSelectionText", 350 * x, ScrH()- (95 * y), Color(255,255,255,255),TEXT_ALIGN_CENTER)
 end
 hook.Add("HUDPaint", "DarkRP_Mod_HUDPaint", paintHud)
