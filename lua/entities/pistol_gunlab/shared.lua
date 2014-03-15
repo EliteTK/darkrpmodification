009cea0e307e4f9483130322e606f880a3107744
markrjg <mark.annable@hotmail.co.uk>
2014-03-15 17:45:52 +0000

    Update shared.lua

--- lua/entities/pistol_gunlab/shared.lua
+++ lua/entities/pistol_gunlab/shared.lua
@@ -1,1 +1,10 @@
---filecreate
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Pistol Gun Lab"
+ENT.Author = "Mark"
+ENT.Spawnable = false
+
+function ENT:SetupDataTables()
+self:NetworkVar("Int",0,"price")
+self:NetworkVar("Entity",1,"owning_ent")
+end
